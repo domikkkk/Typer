10de77f465bba09cf692245d1b051a8053af36df
Dominik <domik141202@gmail.com>
2024-02-02 02:07:12 +0100

    a lot of changes: add saving and reading to file, add division by month

--- Operation_on_guild.py
+++ Operation_on_guild.py
@@ -1,92 +1,112 @@
 import discord
+import numpy as np
+from datetime import datetime
 
 
 class Bets:
-    def __init__(self, guild: discord.Guild) -> None:
-        self._guild = guild
-        self._members = guild.members
+    def __init__(self, guild: dict) -> None:
+        self._guild: discord.Guild = guild['guild']
+        self.allowed_categories = guild["allowed_cat"]
+        self.banned_channels = guild["banned_ch"]
+        self.path = guild["path"]
+        self._members = self._guild.members
         self.members_id = {}
         for member in self._members:
             self.members_id[member.id] = member
         self.bets = {}
-        self.success = ['✅', str(discord.utils.get(guild.emojis, name='success')), '<:7425classiccheckmark:', '<:zielony:']
-        self.failure = ['⛔️', str(discord.utils.get(guild.emojis, name='x_')), '❌']
+        self.success = ['✅', str(discord.utils.get(self._guild.emojis, name='success')), '<:7425classiccheckmark:', '<:zielony:']
+        self.failure = ['⛔️', str(discord.utils.get(self._guild.emojis, name='x_')), '❌']
 
     async def add_mes(self, mes: discord.Message):
-        author = mes.author.nick if mes.author.nick is not None else mes.author.global_name
-        if not author:
-            author = mes.author.name
-        if author not in self.bets:
-            self.bets[author] = {0: [], 1: []}
-        await self.analize_mes(mes)
+        author_id = mes.author.id
+        date = get_year_month(mes.created_at)
+        if author_id not in self.bets:
+            self.bets[author_id] = {date: {0: [], 1: []}}
+        elif date not in self.bets[author_id]:
+            self.bets[author_id][date] = {0: [], 1: []}
+        await self.analize_mes(mes, date)
         
-    async def analize_mes(self, mes: discord.Message):
-        name = mes.author.nick if mes.author.nick is not None else mes.author.global_name
-        if not name:
-            name = mes.author.name
+    async def analize_mes(self, mes: discord.Message, date):
+        author_id = mes.author.id
         if any(succ in mes.content for succ in self.success):
-            self.bets[name][1].append(mes.id)
+            self.bets[author_id][date][1].append(mes.id)
             return
         elif any(fail in mes.content for fail in self.failure):
-            self.bets[name][0].append(mes.id)
+            self.bets[author_id][date][0].append(mes.id)
             return
         for reaction in mes.reactions:
             a = 0
             async for user in reaction.users():
-                if user.id == mes.author.id:
+                if user.id == author_id:
                     a = 1
             if a == 0:
-                return
+                continue
             if str(reaction.emoji) in self.success:
-                self.bets[name][1].append(mes.id)
+                self.bets[author_id][date][1].append(mes.id)
+                break
             elif str(reaction.emoji) in self.failure:
-                self.bets[name][0].append(mes.id)
+                self.bets[author_id][date][0].append(mes.id)
+                break
         return
                 
-
-    def ret_accuracy(self, au:str=None):
+    def ret_accuracy(self, au:str=None, date:str=None):
         if not au:
-            return {member: {"Średnia": self._entropy(member), "Ilość zakładów": self._get_games(member)} for member in self.bets}
+            if not date:
+                data = {}
+                for id in self.bets:
+                    data[self.members_id[id].display_name] = {month: {"Średnia": self._entropy(id, month),
+                                                                      "Ilość zakładów": self._get_games(id, month)}
+                                                              for month in self.bets[id]}
+                return data
+            return {self.members_id[id].display_name: {date: {"Średnia": self._entropy(id, date), "Ilość zakładów": self._get_games(id, date)}}}
         au = au.lower()
-        for member in self.bets:
-            indexes = find_KMP(au, member.lower())
-            if len(indexes) > 0 and len(au)/len(member) > 0.6:
-                au = member
-                break
-        return {au: {"Średnia": self._entropy(au), "Ilość zakładów": self._get_games(au)}}
+        for id in self.bets:
+            name = self.members_id[id].display_name
+            if podciag(au, name) / max(len(au), len(name)) > 0.7:
+                return {name: {"Średnia": self._entropy(id, date), "Ilość zakładów": self._get_games(id, date)}}
+        return {"message": f"Nie znaleziono członka {name}"}
 
-    def _entropy(self, member):
+    def _entropy(self, id, date:str=None):
         try:
-            return round(100* len(self.bets[member][1]) / self._get_games(member), 1)
+            if date is not None:
+                return round(100* len(self.bets[id][date][1]) / self._get_games(id, date), 2)
+            sum_percent = {}
+            for month in self.bets[id]:
+                sum_percent[month] = round(100 * len(self.bets[id][month][1]) / self._get_games(id, month), 2)
+            return sum_percent
         except ZeroDivisionError:
             return 0.0
 
-    def _get_games(self, member):
-        return len(self.bets[member][1]) + len(self.bets[member][0])
+    def _get_games(self, id, date):
+        return len(self.bets[id][date][0]) + len(self.bets[id][date][1])
 
     def _delete_duplicate(self):
-        for member in self.bets:
-            self.bets[member][0] = list(set(self.bets[member][0]))
-            self.bets[member][1] = list(set(self.bets[member][1]))
+        for id in self.bets:
+            for month in self.bets[id]:
+                self.bets[id][month][0] = list(set(self.bets[id][month][0]))
+                self.bets[id][month][1] = list(set(self.bets[id][month][1]))
 
-    async def _synchronized_data(self, allowed_categories, banned_channels):
-        res = ''
+    async def _synchronized_data(self):
         for category in self._guild.categories:
             print(category.name)
-            if category.id in allowed_categories:
+            if category.id in self.allowed_categories:
                 for channel in category.channels:
-                    if channel.id in banned_channels:
+                    if channel.id in self.banned_channels:
                         continue
                     print('\t' + channel.name)
-                    res += channel.name + '\n'
                     await self.analize_history(channel)
-        return res + 'Done'
 
     async def analize_history(self, channel: discord.TextChannel):
         async for message in channel.history(limit=None, oldest_first=True):
             if message.author.bot:
                 continue
             await self.add_mes(message)
+
+    def get_normal_data(self):
+        data = {}
+        for id in self.bets:
+            data[self.members_id[id].display_name] = self.bets[id]
+        return data
 
 
 async def search_mess(id, interaction: discord.Interaction):
@@ -95,42 +115,20 @@ async def search_mess(id, interaction: discord.Interaction):
             return message.content
 
 
-def find_KMP(string, text):
-    lista = []
-    if len(text) - len(string) < 0 or len(string) == 0:
-        return lista
-    m = len(string) #dlugosc wzorca
-    n = len(text)   #dlugosc tekstu
-    i, j = 0, 0     #indeks tekstu, wzorca
-    prefix_arr = prefix(string)
-    while i != n:
-        # print(i, j)
-        if text[i] == string[j]:
-            i += 1
-            j += 1
-        if j == m:
-            lista.append(i - j)
-            j = prefix_arr[j-1]
-        elif i < n and string[j] != text[i]:
-            if j== 0:
-                i+=1
+def get_year_month(date: datetime):
+    return str(date.year) + '-' + str(date.month)
+
+
+def podciag(s1: str, s2: str):
+    s1 = s1.lower()
+    s2 = s2.lower()
+    m = len(s1)
+    n = len(s2)
+    L = np.zeros((m+1, n+1))
+    for i in range(m):
+        for j in range(n):
+            if s1[i] == s2[j]:
+                L[i+1, j+1] = 1 + L[i, j]
             else:
-                j = prefix_arr[j-1]
-    return lista
-
-
-def prefix(pattern):
-    m = len(pattern)  # dlugosc wzorca
-    prefix_arr = [0]*m  # tablica z m zer
-    i, j = 0, 1  # indeksy
-    while j != m:
-        if pattern[j] == pattern[i]:
-            i += 1
-            prefix_arr[j] = i
-            j += 1
-        elif i != 0:
-            i = prefix_arr[i-1]
-        else:
-            prefix_arr[j] = 0
-            j += 1
-    return prefix_arr
+                L[i+1, j+1] = max(L[i+1, j], L[i, j+1])
+    return L[m, n]
