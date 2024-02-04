24e770517ffd4f92975acdbe0e864a0090a30953
Dominik <domik141202@gmail.com>
2024-02-04 21:38:43 +0100

    Add logging

--- Typer.py
+++ Typer.py
@@ -4,12 +4,15 @@ from discord import app_commands
 import discord
 import json
 import asyncio
+import logging
 from datetime import datetime
 from Operation_on_guild import Bets
 from startup import Typer, start, write_to_db, check_date
 
 
 Bet: Bets = None
+logging.basicConfig(filename="Typer.log", level=logging.DEBUG, format='%(levelname)s - %(asctime)s - %(message)s')
+logger = logging.getLogger('Typer_logging')
 
 
 @Typer.event
@@ -32,10 +35,10 @@ async def on_message_edit(before: discord.Message, after: discord.Message):
     global Bet
     if before.author.id == Typer.user.id:
         return
-    print(f"{before.author.display_name} editied message at {datetime.utcnow()} on channel {before.channel.name}")
     if before.content != after.content and after.channel.id not in Bet.banned_channels and after.channel.category_id in Bet.allowed_categories:
         await Bet.add_mes(after)
         Bet._delete_duplicate()
+        logger.info(f"{before.author.display_name} ({before.author.id}) editied message on channel {before.channel.name}. Message id: {after.id}")
 
 
 @Typer.event
@@ -55,13 +58,14 @@ async def on_message_delete(mes: discord.Message):
 
 
 @Typer.event
-async def on_reaction_add(reacion: discord.Reaction, user: discord.Member):
+async def on_reaction_add(reaction: discord.Reaction, user: discord.Member):
     global Bet
     if user.bot:
         return
-    if reacion.message.author.id == user.id and reacion.message.channel.id not in Bet.banned_channels and reacion.message.channel.category_id in Bet.allowed_categories:
-        await Bet.add_mes(reacion.message)
+    if reaction.message.author.id == user.id and reaction.message.channel.id not in Bet.banned_channels and reaction.message.channel.category_id in Bet.allowed_categories:
+        await Bet.add_mes(reaction.message)
         Bet._delete_duplicate()
+        logger.info(f"{reaction.message.author.display_name} ({reaction.message.author.id}) added reaction on channel {reaction.message.channel.name}. Message id: {reaction.message.id}")
 
 
 @Typer.tree.command()
@@ -90,6 +94,7 @@ async def synchronize(interaction: discord.Interaction, date: str=None, channel_
         res = await Bet._synchronized_data(date, channel_id)
         Bet._delete_duplicate()
         write_to_db(Bet.path, Bet.get_normal_data())
+        logger.debug("Zapisano baze danych")
         await interaction.channel.send(res)
     except Exception as e:
         await interaction.response.send_message(e, ephemeral=True)
@@ -116,13 +121,12 @@ async def info_per_month():
     await Typer.wait_until_ready()
     global Bet
     channel_id = 1158130114933047387
-    channel_id = 1114293456127414293
     channel = Typer.get_channel(channel_id)
     while not Typer.is_closed():
         day = datetime.utcnow().day
         print(day)
         if day == 1:
-            print("Comiesięczne info")
+            logger.debug("Co miesięczne info")
             data = await Bet.ret_accuracy()
             for name in data:
                 data_to_show = {name: data[name]}
